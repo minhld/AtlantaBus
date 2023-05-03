91a043e8173e00b3e5b5af38c1cca83388496d6e
mac <mac@macs-MacBook-Pro.local>
2023-05-03 02:38:42 -0400

    added all files

---
+++ App.js
@@ -0,0 +1,11 @@
+import React, {useState} from 'react';
+import {Text, View} from 'react-native';
+
+const App = () => {
+    return (
+        <View>
+            <Text>Hello World</Text>
+        </View>
+    );
+  };
+export default App;
