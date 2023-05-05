52aa66c75ce2d78c2b67502cd6f16a74380f5501
mac <mac@macs-MacBook-Pro.local>
2023-05-05 09:17:57 -0400

    added icons and side menu

--- App.js
+++ App.js
@@ -1,30 +1,56 @@
-// import { Icon } from 'react-native-elements';
 import React from 'react';
-import {Text, View} from 'react-native';
- 
-import { NavigationContainer } from '@react-navigation/native';
-import {createNativeStackNavigator} from '@react-navigation/native-stack';
-import { createDrawerNavigator } from '@react-navigation/drawer';
+import { Icon } from '@rneui/themed';
 
+import { NavigationContainer } from '@react-navigation/native';
+import { createDrawerNavigator } from '@react-navigation/drawer';
 import Home from './pages/Home';
 import Flash from './pages/Flash';
+import BusList from './pages/BusList';
 
-const Stack = createNativeStackNavigator();
 const Drawer = createDrawerNavigator();
+
 const App = () => {
   return (
-    // <View>
-    //   <Text>Welcome to Home</Text>
-    // </View>
     <NavigationContainer>
-      <Stack.Navigator>
-          <Stack.Screen
-            name="Home"
-            component={Home}
-            options={{title: 'Welcome'}}
-          />
-          <Stack.Screen name="Flash" component={Flash} />
-        </Stack.Navigator>
+      <Drawer.Navigator
+        drawerType="front"
+        initialRouteName="Home"
+        screenOptions={{
+          headerStyle: { 
+            backgroundColor: '#3262a8' 
+          },
+          headerTintColor: '#fff',
+          headerTitleStyle: {
+            fontWeight: 'bold',
+          },
+          drawerActiveTintColor: '#3262a8',
+          drawerActiveBackgroundColor: '#eee',
+          drawerInactiveTintColor: '#666',
+          labelStyle: {
+            marginLeft: 0,
+          },
+        }}>
+        <Drawer.Screen
+          name="Home"
+          component={Home}
+          options={{
+            title: 'Home',
+            drawerIcon: ({focused, size}) => (
+              <Icon name="home" color="#3262a8" />
+           ),
+          }}
+        />
+        <Drawer.Screen
+          name="BusList"
+          component={BusList}
+          options={{
+            title: 'Bus List',
+            drawerIcon: ({focused, size}) => (
+              <Icon name="dashboard" color="#3262a8" />
+            ),
+          }}
+        />
+      </Drawer.Navigator>
     </NavigationContainer>
   );
 };
